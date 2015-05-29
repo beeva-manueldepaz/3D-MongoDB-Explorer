b2f68a92d9bc5e47e5c8a1772626ff9bc1966293
Manuel Eusebio de Paz Carmona <manuel.depaz@beeva.com>
2015-05-29 18:31:18 +0200

    Get collections

--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
 var express = require('express');
 var mongodb = require('mongodb');
+var _       = require('lodash');
+
 var app = express();
 
-var MONGODB_URI = 'mongodb://127.0.0.1:27017/salamandra';
-var db;
+var Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    ObjectID = require('mongodb').ObjectID,
+    Binary = require('mongodb').Binary,
+    GridStore = require('mongodb').GridStore,
+    Grid = require('mongodb').Grid,
+    Code = require('mongodb').Code;
+    //BSON = require('mongodb').pure().BSON;
 
-// Initialize connection once
-mongodb.MongoClient.connect(MONGODB_URI, function(err, database) {
+
+var db = new Db('test', new Server('localhost', 27017));
+
+// Establish connection to db
+db.open(function(err, db) {
 
     if (err) { console.log('no mongoDB'); }
 
     else {
 
-        db = database;
+        //db = database;
         var server = app.listen(3001, function () {
 
             var host = server.address().address;
@@ -23,12 +35,54 @@ mongodb.MongoClient.connect(MONGODB_URI, function(err, database) {
     }
 });
 
-app.get('/collection/:id', function(req, res) {
+app.get('/', function (req, res) {
 
-    var id = req.params.id;
+    // Use the admin database for the operation
+    var adminDb = db.admin();
 
+    // List all the available databases
+    adminDb.listDatabases(function(err, dbs) {
+
+        if (err) { res.status(500).send([]); }
+        else {
+
+            var listDbs = [];
+            _.each(dbs.databases,function(item) {
+                listDbs.push(item.name);
+            });
+
+            res.status(200).send(listDbs);
+        }
+
+    });
+});
+
+
+app.get('/:db', function(req, res) {
+
+    var id = req.params.db;
+    var secondDb = db.db(id);
+
+    secondDb.collections(function(err, collections) {
+
+        if (err) { res.status(500).send([]); }
+        else {
+            console.log(collections);
+
+            var listCols = [];
+            _.each(collections,function(item) {
+                listCols.push(item.s.name);
+            });
+
+            res.status(200).send(listCols);
+        }
+
+    });
+
+    /*
     db.collection(id).find({},{limit:10}, function(err, docs) {
         docs.each(function(err, doc) {
+
             if(doc) {
                 res.write(JSON.stringify(doc) + "\n");
             }
@@ -37,8 +91,6 @@ app.get('/collection/:id', function(req, res) {
             }
         });
     });
+    */
 });
 
-app.get('/collections', function (req, res) {
-    res.send('Hello World!');
-});
